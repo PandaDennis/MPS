150b1dd9814b5462cfb1f935b4a9ea478c1f3905
PandaDennis <Dennisdennisbyapple@gmail.com>
2021-06-14 17:55:04 +0800

    update project and add a REGX

--- src/components/page/createProject/c_project.js
+++ src/components/page/createProject/c_project.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2'
 import './c_project.css';
-import { Layout, Form, Input, Button, Row, Col, Typography, Select, Modal ,Switch} from 'antd';
-import { ExclamationCircleOutlined ,CloseOutlined, CheckOutlined} from '@ant-design/icons';
+import { Layout, Form, Input, Button, Row, Col, Typography, Select, Modal, Switch } from 'antd';
+import { ExclamationCircleOutlined, CloseOutlined, CheckOutlined } from '@ant-design/icons';
 
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
@@ -18,7 +18,7 @@ const uuid = uuidv4();
 
 const children = [];
 for (let i = 10; i < 36; i++) {
-  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
 }
 const projectGroup = [];
 for (let i = 10; i < 36; i++) {
@@ -30,7 +30,7 @@ for (let i = 10; i < 36; i++) {
 const submitHandler = (values) => {
     const axios = require('axios');
     var subMitData = [];
-   
+
     subMitData.push(values.project);
     console.log(subMitData);
 
@@ -41,6 +41,21 @@ const submitHandler = (values) => {
             console.log("Testing  :" + response);
             if (response.data.status === 0 && response.status === 200) {
                 console.log(response);
+                // Swal.fire(
+                //     'Project Success to Create',
+                //     'You clicked the button!',
+                //     'success'
+                // )
+                Swal.fire({
+                    title: "Project Success to Create",
+                    text: "Click the button go to a project list.",
+                    icon: "success"
+                }).then(function() {
+                    window.location = "../project";
+                });
+
+                
+                
 
             } else {
                 console.log("Error")
@@ -67,7 +82,7 @@ function Create_project() {
             .catch(err => {
                 console.log(err)
             })
-            
+
     }, [])
     const handleChange = (value) => {
         if (value === 'Change' && changest === false) {
@@ -146,15 +161,14 @@ function Create_project() {
                             <Form.Item
                                 name={['project', 'projectid']}
                                 label="Project ID"
-                                initialValue = {uuid}
+                                initialValue={uuid}
                                 rules={[
                                     {
                                         required: true,
-                                        message: 'Please input the Project Name',
                                     },
                                 ]}
                             >
-                                <Input disabled={true}/>
+                                <Input disabled={true} />
                             </Form.Item>
                             <Form.Item
                                 name={['project', 'projectname']}
@@ -162,11 +176,12 @@ function Create_project() {
                                 rules={[
                                     {
                                         required: true,
-                                        message: 'Please input the Project Name',
+                                        message: 'Please input the Project Name, The Project name only Text and number ',
+                                        pattern: /^[A-Z a-z 0-9]+$/
                                     },
                                 ]}
                             >
-                                <Input/>
+                                <Input />
                             </Form.Item>
                             <Form.Item
                                 label="Owner"
@@ -184,7 +199,7 @@ function Create_project() {
                                         {projectownUser.map(item => <Select.Option value={item.name} key={item.id}>{item.name}</Select.Option>)}
                                     </Select.OptGroup>
                                     <Select.OptGroup label="Other User">
-                                        <Select.Option key="Change" >{changest ?  "Go Back to Owner User" : "Change User" }</Select.Option>
+                                        <Select.Option key="Change" >{changest ? "Go Back to Owner User" : "Change User"}</Select.Option>
                                     </Select.OptGroup>
                                 </Select>
 
@@ -209,7 +224,7 @@ function Create_project() {
                                     // defaultValue={}
                                     onChange={handleChange}
                                     style={{ width: '100%' }}
-                                    >
+                                >
                                     {projectGroup}
                                 </Select>
 
@@ -234,25 +249,25 @@ function Create_project() {
                                     // defaultValue={}
                                     onChange={handleChange}
                                     style={{ width: '100%' }}
-                                    >
+                                >
                                     {children}
                                 </Select>
 
 
 
                             </Form.Item>
-                            
-                            <Form.Item 
-                            label="Status"
-                            name={['project', 'projectStatus']}
-                            valuePropName="checked"
-                            initialValue = {true}
+
+                            <Form.Item
+                                label="Status"
+                                name={['project', 'projectStatus']}
+                                valuePropName="checked"
+                                initialValue={true}
                             >
-                            
-                            <Switch
-                                checkedChildren={<CheckOutlined />}
-                                unCheckedChildren={<CloseOutlined />}
-                            />
+
+                                <Switch
+                                    checkedChildren={<CheckOutlined />}
+                                    unCheckedChildren={<CloseOutlined />}
+                                />
 
                             </Form.Item>
 
--- src/components/page/projectList/ProjectList.js
+++ src/components/page/projectList/ProjectList.js
@@ -1,116 +1,161 @@
 import React, { Component } from 'react';
-import { Button,Typography, Layout,Tabs,Avatar,Spin ,Row, Col,Empty} from 'antd';
+import { Button, Typography, Layout, Tabs, Avatar, Spin, Row, Col, Empty } from 'antd';
 import axios from 'axios';
-const { Title ,Text} = Typography;
-const { Content ,Footer } = Layout;
+const { Title, Text } = Typography;
+const { Content, Footer } = Layout;
 const { TabPane } = Tabs;
 
 export class projectList extends Component {
   constructor() {
     super();
-    this.state = ({ 
+    this.state = ({
       project: [],
-      my_dataDisplay:false
+      my_dataDisplay: false
     });
   }
 
   componentDidMount() {
     // Simple GET request using axios
     axios.get('/project/findAll')
-        .then(
-          response => {
-            // console.log(response.data.message.length)
-            // console.log(response.data)
-            if(response.data.status === 1){
-              
-              this.setState({
-                my_dataDisplay:false
-              }) 
-            }else{
-              this.setState({
-                project: response.data,
-                my_dataDisplay:true
-              })
-            }
-            // this.setState(
-            //   { 
-            //     project: response.data.message
-            //   }),
-          })
-          
-          
-      
-    }
-  
- 
+      .then(
+        response => {
+          // console.log(response.data.message.length)
+          // console.log(response.data)
+          if (response.data.status === 1) {
+
+            this.setState({
+              my_dataDisplay: false
+            })
+          } else {
+            this.setState({
+              project: response.data,
+              my_dataDisplay: true
+            })
+          }
+          // this.setState(
+          //   { 
+          //     project: response.data.message
+          //   }),
+        })
+
+
+
+  }
+
+
 
   render() {
 
     const { project } = this.state;
-    if(this.state.my_dataDisplay){
+    if (this.state.my_dataDisplay) {
       return (
         <>
-         
           <Layout style={{ position: 'fixed', zIndex: 1, width: '100%', height: '100%' }}>
-            
-          <Content className="site-layout" style={{ padding: "0 150px", marginTop: -3 }}>
+
+            <Content className="site-layout" style={{ padding: "0 150px", marginTop: -3 }}>
               <div style={{ padding: 24, minHeight: 380, width: "100%", height: "100%" }}>
                 {/* <Title level={3}>Project</Title> */}
                 <div>
                   <Row>
                     <Col span={8}><Title level={4}>Project</Title></Col>
-                    <Col span={8} style={{textAlign:"right"}} offset={8}>
-                    <Button type="dashed" style={{ background: "red", borderColor: "yellow" }} href='/project/create'>
-                         Create
-                    </Button>
+                    <Col span={8} style={{ textAlign: "right" }} offset={8}>
+                      <Button type="dashed" href='/project/create'>
+                        Create
+                      </Button>
                     </Col>
                   </Row>
                 </div>
-                <hr/>
+                <hr />
                 {/* <Divider /> */}
                 <Tabs defaultActiveKey="1" >
                   <TabPane tab="My Project" key="1">
-                  {
-                    
-                    project.length === 0 && project.status !== 0
-                      ? <Spin />
-                      : project.message.map(project => (
-                        <div key="{project}">
-                          <table width='180' >
-                            <tbody>
-                            <tr>
-                                <td rowSpan='2'>
-                                <Avatar.Group>
-                                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                  <Avatar
-                                    style={{
-                                      backgroundColor: '#f56a00',
-                                    }}
-                                  >
-                                    K
-                                  </Avatar>
-                                </Avatar.Group>
-                                </td>
-                                <td style={{verticalAlign:'top'}}>
-                                  <a style={{paddingLeft:15,color:'black'}} href='www.google.com'>
-                                    {project.project_name}
-                                  </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                  <td style={{verticalAlign:'top',paddingLeft:15.5}}>
-                                  <Text type="secondary">Dis</Text>
-                                  </td>
-                            </tr>
-                            </tbody>
-                          </table>
-                    
-                      </div>
-                      )) 
-                  }
+                    <div>
+                      <table width='300'>
+                        <tbody>
+                          {
+                            project.length === 0 && project.status !== 0
+                              ? <Spin />
+                              : project.message.map(project => (
+                                <p>
+                                  <tr>
+                                    <td rowSpan='2'>
+                                      <Avatar.Group>
+                                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                        <Avatar
+                                          style={{
+                                            backgroundColor: '#f56a00',
+                                          }}
+                                        >
+                                          K
+                                        </Avatar>
+                                      </Avatar.Group>
+                                    </td>
+                                    <td style={{ verticalAlign: 'top', paddingLeft: 15.5, }}>
+                                      <a href='www.google.com'>
+                                        {project.project_name}
+                                      </a>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td style={{ verticalAlign: 'top', paddingLeft: 15.5 }}>
+                                      <Text type="secondary">Dis</Text>
+                                    </td>
+                                  </tr>
+
+                                </p>
+
+                              ))
+
+                          }
+                        </tbody>
+                      </table>
+
+                    </div>
+                    {
+
+                      // project.length === 0 && project.status !== 0
+                      //   ? <Spin />
+                      //   : project.message.map(project => (
+                      //     <div key={project.project_id.toString()}>
+                      //       <table width='180' >
+                      //         <tbody>
+                      //         <tr>
+                      //             <td rowSpan='2'>
+                      //             <Avatar.Group>
+                      //               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                      //               <Avatar
+                      //                 style={{
+                      //                   backgroundColor: '#f56a00',
+                      //                 }}
+                      //               >
+                      //                 K
+                      //               </Avatar>
+                      //             </Avatar.Group>
+                      //             </td>
+                      //             <td style={{verticalAlign:'top',paddingLeft:15.5 }}>
+                      //               <a  href='www.google.com'>
+                      //                 {project.project_name}
+                      //               </a>
+                      //             </td>
+                      //         </tr>
+                      //         <tr>
+                      //               <td style={{verticalAlign:'top',paddingLeft:15.5}}>
+                      //               <Text type="secondary">Dis</Text>
+                      //               </td>
+                      //         </tr>
+                      //         </tbody>
+                      //       </table>
+
+                      //   </div>
+                      //   )) 
+
+
+
+                    }
                     {/* <div>
                       <table width='180'>
                         <tbody>
+                          
                         <tr>
                             <td rowSpan='2'>
                             <Avatar.Group>
@@ -137,7 +182,7 @@ export class projectList extends Component {
                       </table>
                  
                   </div> */}
-                  
+
                   </TabPane>
                   <TabPane tab="Team Project" key="2">
                     apple
@@ -145,52 +190,52 @@ export class projectList extends Component {
                 </Tabs>
               </div>
             </Content>
-           
+
           </Layout>
-  
+
           <Footer style={{ textAlign: 'center' }}>MMPS ©2018 Created by Dennis,Power By </Footer>
         </>
       );
-    }else{
-      return(
+    } else {
+      return (
         <>
-        <Layout style={{ position: 'fixed', zIndex: 1, width: '100%', height: '100%' }}>
-          
-        <Content className="site-layout" style={{ padding: "0 150px", marginTop: -3 }}>
-            <div style={{ padding: 24, minHeight: 380, width: "100%", height: "100%" }}>
-              {/* <Title level={3}>Project</Title> */}
-              <div>
-                <Row>
-                  <Col span={8}><Title level={4}>Project</Title></Col>
-                  <Col span={8} style={{textAlign:"right"}} offset={8}>
-                  <Button type="primary"  href='/project/create'>
-                        New Project
-                    </Button>
-                  </Col>
-                </Row>
-              </div>
-              <hr/>
-              {/* <Divider /> */}
-              <Tabs defaultActiveKey="1" >
-                <TabPane tab="My Project" key="1">
-                
-                  <Empty />
-                
-                </TabPane>
-                <TabPane tab="Team Project" key="2">
-                  <Empty />
-                </TabPane>
-              </Tabs>
-            </div>
-          </Content>
-         
-        </Layout>
+          <Layout style={{ position: 'fixed', zIndex: 1, width: '100%', height: '100%' }}>
 
-        <Footer style={{ textAlign: 'center' }}>MMPS ©2018 Created by Dennis,Power By </Footer>
-      </>
+            <Content className="site-layout" style={{ padding: "0 150px", marginTop: -3 }}>
+              <div style={{ padding: 24, minHeight: 380, width: "100%", height: "100%" }}>
+                {/* <Title level={3}>Project</Title> */}
+                <div>
+                  <Row>
+                    <Col span={8}><Title level={4}>Project</Title></Col>
+                    <Col span={8} style={{ textAlign: "right" }} offset={8}>
+                      <Button type="primary" href='/project/create'>
+                        New Project
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+                <hr />
+                {/* <Divider /> */}
+                <Tabs defaultActiveKey="1" >
+                  <TabPane tab="My Project" key="1">
+
+                    <Empty />
+
+                  </TabPane>
+                  <TabPane tab="Team Project" key="2">
+                    <Empty />
+                  </TabPane>
+                </Tabs>
+              </div>
+            </Content>
+
+          </Layout>
+
+          <Footer style={{ textAlign: 'center' }}>MMPS ©2018 Created by Dennis,Power By </Footer>
+        </>
       );
     }
-    
+
   }
 }
 
