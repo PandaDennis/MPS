e0d0a1bffe8ed0cd00606a2388a90babe31f44dd
PandaDennis <Dennisdennisbyapple@gmail.com>
2021-07-10 22:22:57 +0800

    update DEV package and Add a design

--- src/components/page/projectPage_c/projectPage.js
+++ src/components/page/projectPage_c/projectPage.js
@@ -1,59 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import './projectPage.css'
-import { Layout, Menu } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-
+import { Layout, Menu, Avatar } from 'antd';
+import {
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+} from '@ant-design/icons';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+// import P_tree from "./projectTree"
 
-import P_tree from "./projectTree"
 
-const { SubMenu } = Menu;
-const { Content, Sider } = Layout;
+const { Sider } = Layout;
+
 
 
 function ProjectPage() {
-    const { projectId } = useParams();
-    
-   
-    return (
-        <>
+  const { projectId } = useParams();
+  const [projectDetail, setprojectDetail] = useState([])
   
-    <Layout style={{ position: 'fixed', zIndex: 1, width: '100%', height: '100%' }}>
-      <Sider  className="site-layout-background">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-        //   style={{ height: '100%', borderRight: 0 }}
-        >
-          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-            <Menu.Item key="1">option1</Menu.Item>
-            <Menu.Item key="2">option2</Menu.Item>
-            <Menu.Item key="3">option3</Menu.Item>
-            <Menu.Item key="4">option4</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-            <Menu.Item key="9">option9</Menu.Item>
-            <Menu.Item key="10">option10</Menu.Item>
-            <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        {/* <Breadcrumb style={{ margin: '16px 0' }}>
+  useEffect(() => {
+    axios
+      .get('/project/findByID/'+projectId)
+      .then(res => {
+        setprojectDetail(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  })
+
+function getFirstDig(a){
+  if(typeof a !== 'undefined'){
+    return a.charAt(0);
+  }else{
+    return null;
+  }
+}
+
+  return (
+    <>
+
+      <Layout style={{ position: 'fixed', zIndex: 1, width: '100%', height: '100%' }}>
+        <Sider className="site-layout-background">
+        <Avatar
+              key="1"
+              style={{
+                color: '#f56a00',
+                backgroundColor: '#fde3cf',
+                margin: "25px"
+              }}
+            >
+              {getFirstDig(projectDetail.project_name)}
+            </Avatar>
+            {projectDetail.project_name}
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+          //   style={{ height: '100%', borderRight: 0 }}
+          >
+            
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<ContainerOutlined />}>
+              Option 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          {/* <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb> */}
-        {/* <Content
+          {/* <Content
           className="site-layout-background"
           style={{
             padding: 24,
@@ -64,14 +91,14 @@ function ProjectPage() {
           <P_tree/>
           <br/>{projectId}
         </Content> */}
-        <P_tree/>
+           
+        </Layout>
       </Layout>
-    </Layout>
 
 
 
-        </>
-    );
+    </>
+  );
 
 
 
--- src/components/page/projectPage_c/projectTree.js
+++ src/components/page/projectPage_c/projectTree.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import { Tree } from 'antd';
 import { ContainerOutlined} from '@ant-design/icons';
